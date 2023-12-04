8459544af1e56d7774498a23b9e9bc4be5dc987b
Luke O'Connell <147653898+lukeoc615@users.noreply.github.com>
2023-12-04 17:43:26 +0000

    completed exercise

--- birthday_cake.rb
+++ birthday_cake.rb
@@ -19,7 +19,7 @@ Birthday cake with 10 blown out candles
 
 =end
 
-require "active_support"
+require "active_support/all"
 
 class BirthdayCake
   attr_accessor :age, :lit
@@ -38,7 +38,7 @@ class BirthdayCake
   end
 
   def greet
-    "Happy #{self.age.ordinalize} Birdday!"
+    "Happy #{self.age.ordinalize} Birthday!"
   end
 
   def sing
@@ -49,17 +49,25 @@ class BirthdayCake
     "Birthday cake with #{self.age} #{candles_status} candles"
   end
 
+  def light_candles
+    self.lit = true
+  end
+
+  def blow_out_candles
+    self.lit = false
+  end
+
   def BirthdayCake.celebrate(age)
-    birthday_cake = Birthdaycake.new(age)
+    birthday_cake = BirthdayCake.new(age)
     puts birthday_cake.greet
     puts birthday_cake.to_s
     puts "Lighting candles..."
     birthday_cake.light_candles
     puts birthday_cake.to_s
     puts "Singing happy birthday..."
-    puts birthday_cake.sign
+    puts birthday_cake.sing
     puts "Blowing out candles..."
-    birth_cake.blow_out_candles
+    birthday_cake.blow_out_candles
     puts birthday_cake.to_s
 
     birthday_cake
